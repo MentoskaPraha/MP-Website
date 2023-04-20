187ca1f3ee565c6d0007428620bc483cf5a7e213
MentoskaPraha <filipjanstary@gmail.com>
2023-04-20 09:05:12 +0300

    Updated settings, to be able to build

--- svelte.config.js
+++ svelte.config.js
@@ -12,8 +12,10 @@ const config = {
 	kit: {
 		adapter: adapter({
 			pages: "build",
-			assets: "build",
-			fallback: null
+            assets: "build",
+            fallback: "index.html",
+            precompress: false,
+            strict: false
 		})
 	}
 };
