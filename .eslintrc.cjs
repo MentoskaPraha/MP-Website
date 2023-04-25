8a0fd461ccadd810713ecc50b247f69197960ac0
MentoskaPraha <filipjanstary@gmail.com>
2023-04-25 14:25:57 +0300

    Fixed linting issues

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,26 +1,22 @@
 module.exports = {
+	env: {
+		browser: true,
+		es2021: true
+	},
 	extends: [
-		"plugin:astro/recommended",
 		"eslint:recommended",
 		"plugin:@typescript-eslint/recommended",
 		"prettier"
 	],
-	overrides: [
-		{
-			files: ["*.astro", "*.svelte"],
-			plugins: ["svelte3", "@typescript-eslint"],
-			parser: "astro-eslint-parser",
-			parserOptions: {
-				parser: "@typescript-eslint/parser",
-				extraFileExtensions: [".astro", ".svelte"],
-				sourceType: "module",
-				ecmaVersion: 2020
-			},
-			env: {
-				browser: true,
-				es2017: true,
-				node: true
-			}
-		}
-	]
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		ecmaVersion: 12,
+		sourceType: "module"
+	},
+
+	plugins: ["svelte3", "@typescript-eslint", "prettier"],
+
+	rules: {
+		"prettier/prettier": "error"
+	}
 };
