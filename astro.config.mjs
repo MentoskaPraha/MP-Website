46caf28686fa80e8e4d7fb8192cc918a4b9747ff
MentoskaPraha <contact+git@mentoskapraha.com>
2026-04-13 12:46:32 -0500

    Setup for cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,18 @@ import sitemap from "@astrojs/sitemap";
 import svelte from "@astrojs/svelte";
 import tailwindcss from "@tailwindcss/vite";
 
+import cloudflare from "@astrojs/cloudflare";
+
 export default defineConfig({
   site: "https://www.mentoskapraha.com",
   trailingSlash: "never",
   output: "static",
   integrations: [sitemap(), svelte()],
   prefetch: true,
+
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  adapter: cloudflare()
 });
